a80af88b42b19ea5ff829acc886ec0ff56d9acf5
Dominic Ross <youngdad33@gmail.com>
2023-07-04 08:42:19 +0100

    managed to return list of film titles and posters, but that has no information about the film will probably need another fetch for movie details

--- index.js
+++ index.js
@@ -4,28 +4,32 @@ const searchForm = document.getElementById('search');
 searchForm.addEventListener('submit', (e) => {
   e.preventDefault();
   const searchEntry = document.getElementById('search-input').value;
-  fetch(`http://www.omdbapi.com/?apikey=57c7dfb6&t=${searchEntry}&page=5`)
+  fetch(`http://www.omdbapi.com/?apikey=57c7dfb6&s=${searchEntry}&page=1`)
     .then((res) => res.json())
     .then((data) => {
       console.log(data);
-      searchResults.innerHTML = `
-      <div class="result-container">
-      <img src=${data.Poster} alt="${data.Title}">
-      <section>
-        <div class="header">
-          <h3>${data.Title}</h3>
-          <img src="img/icon-star.png" alt="star icon" class="icon">
-          <p>${data.imdbRating}</p>
+      searchResults.innerHTML = '';
+      for (let movie of data.Search) {
+        searchResults.innerHTML += `
+        <div class="result-container">
+        <img src=${movie.Poster} alt="${movie.Title}">
+        <section>
+          <div class="header">
+            <h3>${movie.Title}</h3>
+            <img src="img/icon-star.png" alt="star icon" class="icon">
+            <p>${movie.imdbRating}</p>
+          </div>
+          <div class="movie-data">
+            <p>${movie.Runtime}</p>
+            <p>${movie.Genre}</p>
+            <img src="img/icon-plus.png" alt="plus icon" class="icon">
+            <p>Watchlist</p>
+          </div>
+          <p>${movie.Plot}</p>
+        </section>
         </div>
-        <div class="movie-data">
-          <p>${data.Runtime}</p>
-          <p>${data.Genre}</p>
-          <img src="img/icon-plus.png" alt="plus icon" class="icon">
-          <p>Watchlist</p>
-        </div>
-        <p>${data.Plot}</p>
-      </section>
-      </div>
-      `;
+        <hr>
+        `;
+      }
     });
 });
