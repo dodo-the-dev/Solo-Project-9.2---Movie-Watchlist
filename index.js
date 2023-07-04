121bb802945f6617e18055250c3d473275d95774
Dominic Ross <youngdad33@gmail.com>
2023-07-04 07:51:45 +0100

    styled the results, but only getting one result returned

--- index.js
+++ index.js
@@ -4,22 +4,23 @@ const searchForm = document.getElementById('search');
 searchForm.addEventListener('submit', (e) => {
   e.preventDefault();
   const searchEntry = document.getElementById('search-input').value;
-  fetch(`http://www.omdbapi.com/?apikey=57c7dfb6&t=${searchEntry}`)
+  fetch(`http://www.omdbapi.com/?apikey=57c7dfb6&t=${searchEntry}&page=5`)
     .then((res) => res.json())
     .then((data) => {
+      console.log(data);
       searchResults.innerHTML = `
       <div class="result-container">
       <img src=${data.Poster} alt="${data.Title}">
       <section>
         <div class="header">
           <h3>${data.Title}</h3>
-          <img src="img/icon-star.png" alt="star icon">
+          <img src="img/icon-star.png" alt="star icon" class="icon">
           <p>${data.imdbRating}</p>
         </div>
         <div class="movie-data">
           <p>${data.Runtime}</p>
           <p>${data.Genre}</p>
-          <img src="img/icon-plus.png">
+          <img src="img/icon-plus.png" alt="plus icon" class="icon">
           <p>Watchlist</p>
         </div>
         <p>${data.Plot}</p>
