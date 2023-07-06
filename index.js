04c9d52877802ab94b9061586b765e475f28c5ca
Dominic Ross <youngdad33@gmail.com>
2023-07-06 08:27:51 +0100

    final styling and code clear up of movie list

--- index.js
+++ index.js
@@ -7,17 +7,14 @@ searchForm.addEventListener('submit', (e) => {
   fetch(`http://www.omdbapi.com/?apikey=57c7dfb6&s=${searchEntry}&page=1`)
     .then((res) => res.json())
     .then((data) => {
-      console.log(data.Search);
       const movieIDs = data.Search.map(function(movie){
         return movie.imdbID
       })
-      console.log(movieIDs)
       searchResults.innerHTML = '';
       for (let id of movieIDs){
         fetch(`http://www.omdbapi.com/?apikey=57c7dfb6&i=${id}`)
           .then(resp => resp.json())
           .then(data => {
-            console.log(data)
             searchResults.innerHTML += `
               <div class="result-container">
               <img src=${data.Poster} alt="${data.Title}">
@@ -30,13 +27,14 @@ searchForm.addEventListener('submit', (e) => {
                 <div class="movie-data">
                   <p>${data.Runtime}</p>
                   <p>${data.Genre}</p>
-                  <img src="img/icon-plus.png" alt="plus icon" class="icon">
-                  <p>Watchlist</p>
+                  <div id="add-to-watchlist">
+                    <img src="img/icon-plus.png" alt="plus icon" class="icon">
+                    <p>Watchlist</p>
+                  </div>
                 </div>
                 <p>${data.Plot}</p>
               </section>
               </div>
-              <hr>
               `})
             }
         })
