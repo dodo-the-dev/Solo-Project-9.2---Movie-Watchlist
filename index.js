3201913e06495c8bf7b37da4d1dfd4ff49c787a0
Dominic Ross <youngdad33@gmail.com>
2023-07-13 08:46:37 +0100

    started the process of adding films to local storage as an array so I can delete them and display them

--- index.js
+++ index.js
@@ -2,47 +2,23 @@ const searchResults = document.getElementById('search-results');
 const searchForm = document.getElementById('search');
 // const addToWatchlist = document.getElementsByClassName('add-to-watchlist');
 
-searchForm.addEventListener('submit', (e) => {
-  e.preventDefault();
-  const searchEntry = document.getElementById('search-input').value;
-  fetch(`http://www.omdbapi.com/?apikey=57c7dfb6&s=${searchEntry}&page=1`)
-    .then((res) => res.json())
-    .then((data) => {
-      const movieIDs = data.Search.map(function (movie) {
-        return movie.imdbID;
-      });
-      searchResults.innerHTML = '';
-      for (let id of movieIDs) {
-        fetch(`http://www.omdbapi.com/?apikey=57c7dfb6&i=${id}`)
-          .then((resp) => resp.json())
-          .then((data) => {
-            console.log(data);
-            searchResults.innerHTML += `
-              <div class="result-container">
-              <img src=${data.Poster} alt="${data.Title}">
-              <section>
-                <div class="header">
-                  <h3>${data.Title}</h3>
-                  <img src="img/icon-star.png" alt="star icon" class="icon">
-                  <p>${data.imdbRating}</p>
-                </div>
-                <div class="movie-data">
-                  <p>${data.Runtime}</p>
-                  <p>${data.Genre}</p>
-                  <div class="add-to-watchlist" id="${data.imdbID}">
-                    <img src="img/icon-plus.png" alt="plus icon" class="add-to-watchlist">
-                    <p class="add-to-watchlist">Watchlist</p>
-                  </div>
-                </div>
-                <p>${data.Plot}</p>
-              </section>
-              </div>
-              `;
-          });
-      }
-    });
-});
-let watchlistFilms = [];
+// searchForm.addEventListener('submit', (e) => {
+//   e.preventDefault();
+//   const searchEntry = document.getElementById('search-input').value;
+//   fetch(`http://www.omdbapi.com/?apikey=57c7dfb6&s=${searchEntry}&page=1`)
+//     .then((res) => res.json())
+//     .then((data) => {
+//       const movieIDs = data.Search.map(function (movie) {
+//         return movie.imdbID;
+//       });
+//       // console.log(movieIDs)
+//       searchResults.innerHTML = '';
+//       renderFilmList(movieIDs)
+//       }
+//     );
+//     })
+
+let movieWatchList = []
 
 document.addEventListener('click', function (e) {
   if (e.target.className === `add-to-watchlist`) {
@@ -52,13 +28,50 @@ document.addEventListener('click', function (e) {
       <img class="remove-from-watchlist" src="img/icon-minus.png" alt="icon-minus">
       <p class="remove-from-watchlist">Remove</p>
     `;
-    localStorage.setItem(`id-${targetId}`, targetId)
+    movieWatchList.push(targetId)
+    localStorage.setItem("movieWatchlist",JSON.stringify(movieWatchList))
   } else if(e.target.className === "remove-from-watchlist"){
     let targetId = e.target.parentElement.id
     e.target.parentElement.innerHTML =`
     <img src="img/icon-plus.png" alt="plus icon" class="add-to-watchlist">
     <p class="add-to-watchlist">Watchlist</p>
     `
+
+    //TODO: use the filter method to find and remove the desired film ID (arr = arr.filter(item => item !== value))
+    movieWatchList.find(id => id===targetId)
     localStorage.removeItem(`id-${targetId}`, targetId)
   }
 });
+
+
+function renderFilmList(movieIDs){
+  for (let id of movieIDs) {
+    fetch(`http://www.omdbapi.com/?apikey=57c7dfb6&i=${id}`)
+      .then((resp) => resp.json())
+      .then((data) => {
+        console.log(data);
+        searchResults.innerHTML += `
+          <div class="result-container">
+          <img src=${data.Poster} alt="${data.Title}">
+          <section>
+            <div class="header">
+              <h3>${data.Title}</h3>
+              <img src="img/icon-star.png" alt="star icon" class="icon">
+              <p>${data.imdbRating}</p>
+            </div>
+            <div class="movie-data">
+              <p>${data.Runtime}</p>
+              <p>${data.Genre}</p>
+              <div class="add-to-watchlist" id="${data.imdbID}">
+                <img src="img/icon-plus.png" alt="plus icon" class="add-to-watchlist">
+                <p class="add-to-watchlist">Watchlist</p>
+              </div>
+            </div>
+            <p>${data.Plot}</p>
+          </section>
+          </div>
+          `;
+      });
+}}
+
+export{renderFilmList}
---
+++ watchlist.js
@@ -0,0 +1,8 @@
+import { renderFilmList } from "/index.js";
+window.onload = (e) =>{
+  let movieIDs = []
+  //TODO: recover array from local storage and iterate over it using renderFilmList method
+  for( id of localStorage ){
+    JSON.parse(localStorage.getItem)
+  }
+}
