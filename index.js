874843bf14ed708416acaeaf549527c3c485dd8d
Dominic Ross <youngdad33@gmail.com>
2023-07-13 07:32:57 +0100

    got the add and remove to watchlist buttons working and saving to local storage

--- index.js
+++ index.js
@@ -48,14 +48,17 @@ document.addEventListener('click', function (e) {
   if (e.target.className === `add-to-watchlist`) {
     let targetId = e.target.parentElement.id;
     console.log(targetId);
-    document.getElementById(e.target.parentElement.id).innerHTML = `
+    e.target.parentElement.innerHTML = `
       <img class="remove-from-watchlist" src="img/icon-minus.png" alt="icon-minus">
       <p class="remove-from-watchlist">Remove</p>
     `;
-    // console.log(targetId);
-    watchlistFilms.push(targetId);
-    console.log(watchlistFilms);
-  } else {
-    console.log('click elsewhere');
+    localStorage.setItem(`id-${targetId}`, targetId)
+  } else if(e.target.className === "remove-from-watchlist"){
+    let targetId = e.target.parentElement.id
+    e.target.parentElement.innerHTML =`
+    <img src="img/icon-plus.png" alt="plus icon" class="add-to-watchlist">
+    <p class="add-to-watchlist">Watchlist</p>
+    `
+    localStorage.removeItem(`id-${targetId}`, targetId)
   }
 });
